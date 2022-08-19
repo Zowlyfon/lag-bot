3339fb2ddafad2f4c351664903b2f6af78f3eb36
Zowlyfon <drew@zowlyfon.net>
2022-08-19 11:49:24 +0200

    ESLint Prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,14 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier',
+    ],
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error'
+    },
     root: true,
 };
