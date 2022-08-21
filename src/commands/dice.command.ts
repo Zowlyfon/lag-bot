92af15098dd761021452ed494ee0e10727c70d69
Andrew Hall <Andrew.Hall649@gmail.com>
2022-08-21 12:16:33 +1000

    improve dice command; add support for multiple dice, clean up some strings, rename sides subcommand

--- src/commands/dice.command.ts
+++ src/commands/dice.command.ts
@@ -11,14 +11,32 @@ export default class DiceCommand implements CommandInterface {
 
     async runCommand(interaction: ChatInputCommandInteraction) {
         let sides = 6;
+        let dice = 1;
+        let diceMax = 100; // We probably don't want someone rolling a thousand dice.
+
         if (interaction.options.getSubcommand() === 'sides') {
             const sidesOption = interaction.options.getNumber('sides');
             if (sidesOption) {
                 sides = Math.min(sidesOption);
             }
+            const diceOption = interaction.options.getNumber('dice');
+            if (diceOption) {
+                dice = Math.min(Math.max(diceOption, 1), diceMax); // clamp between 1 and diceMax
+            }
         }
-        const roll = Math.floor(Math.random() * sides) + 1;
-        await interaction.reply('You rolled ' + roll + ' on a ' + sides + ' sided die!');
+
+        let rolls = [];
+        for (let i = 0; i < dice; i++) {
+            rolls.push(Math.floor(Math.random() * sides) + 1);
+        }
+
+        const total = rolls.reduce((rollsSum, r) => rollsSum + r, 0);
+
+        if (dice == 1) {
+            await interaction.reply(`You rolled ${rolls[0]} on a ${sides} sided die!`);
+        } else {
+            await interaction.reply(`You rolled ${dice} dice with ${sides} sides! Results: ${rolls.join(", ")}\nTotal of all roles: ${total}`);
+        }
     }
 
     slashCommandBuilder(): SlashCommandSubcommandsOnlyBuilder | SlashCommandBuilder {
@@ -27,10 +45,13 @@ export default class DiceCommand implements CommandInterface {
             .setDescription('Roll a dice')
             .addSubcommand((subcommand) =>
                 subcommand
-                    .setName('sides')
-                    .setDescription('Set a number of sides')
+                    .setName('roll')
+                    .setDescription('Rolls one or more dice with a user-defined number of sides.')
                     .addNumberOption((option) =>
-                        option.setName('sides').setDescription('The number of sides').setRequired(true),
+                        option.setName('sides').setDescription('The number of sides').setRequired(true)
+                    )
+                    .addNumberOption((option) =>
+                        option.setName('dice').setDescription('The number of dice').setRequired(false)
                     ),
             );
     }
