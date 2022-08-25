4a929731f18506a26e86459b51e755a3f3d36f4a
Zowlyfon <drew@zowlyfon.net>
2022-08-25 19:42:17 +0200

    Geocoded weather command

--- src/commands/weather.command.ts
+++ src/commands/weather.command.ts
@@ -3,7 +3,13 @@ import WeatherService from '../services/weather.service';
 import { SubCommand } from '../sub-command.type';
 import { InteractionHandler } from '../interaction-handler.type';
 import { Subject } from 'rxjs';
-import { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from 'discord.js';
+import {
+    ChatInputCommandInteraction,
+    Colors,
+    EmbedBuilder,
+    SlashCommandBuilder,
+    SlashCommandSubcommandsOnlyBuilder,
+} from 'discord.js';
 import { Service } from 'typedi';
 
 @Service()
@@ -16,32 +22,50 @@ export default class WeatherCommand implements CommandInterface {
     constructor(private weatherService: WeatherService) {}
 
     async init() {
-        this.subCommands.push({
-            command: 'coords',
-            runCommand: async (interaction) => {
-                const lon = interaction.options.getNumber('lon');
-                const lat = interaction.options.getNumber('lat');
+        this.subCommands.push(
+            {
+                command: 'coords',
+                runCommand: async (interaction) => {
+                    const lon = interaction.options.getNumber('lon');
+                    const lat = interaction.options.getNumber('lat');
 
-                if (!lon || !lat) return;
+                    if (!lon || !lat) return;
 
-                try {
-                    const result = await this.weatherService.getCurrentWeather(lat, lon);
+                    await interaction.deferReply();
 
-                    if (!result) {
-                        await interaction.reply({ content: 'Could not fetch weather :(', ephemeral: true });
-                        return;
-                    }
-
-                    const weather = result.data;
-
-                    await interaction.reply(
-                        `Temp: ${weather.main.temp}, Feels Like: ${weather.main.feels_like}, Temp Min: ${weather.main.temp_min}, Temp Max: ${weather.main.temp_max}, Pressure: ${weather.main.pressure}, Humidity: ${weather.main.humidity}`,
-                    );
-                } catch (e) {
-                    console.error(e);
-                }
+                    await this.getWeather(lat, lon, interaction);
+                },
             },
-        });
+            {
+                command: 'city',
+                runCommand: async (interaction) => {
+                    const city = interaction.options.getString('city');
+
+                    if (!city) return;
+                    await interaction.deferReply();
+                    try {
+                        const geo = await this.weatherService.geoCoding(city);
+                        console.log(geo);
+
+                        if (!geo || !geo.data) {
+                            await interaction.editReply('City not found');
+                            return;
+                        }
+                        const foundCity = geo.data.pop();
+                        if (!foundCity) {
+                            await interaction.editReply('City not found');
+                            return;
+                        }
+                        const lat = foundCity.lat;
+                        const lon = foundCity.lon;
+
+                        return await this.getWeather(lat, lon, interaction);
+                    } catch (e) {
+                        console.error(e);
+                    }
+                },
+            },
+        );
     }
 
     async runCommand() {
@@ -58,6 +82,48 @@ export default class WeatherCommand implements CommandInterface {
                     .setDescription('Get the weather at the given coordinates')
                     .addNumberOption((option) => option.setName('lat').setDescription('Latitude').setRequired(true))
                     .addNumberOption((option) => option.setName('lon').setDescription('Longitude').setRequired(true)),
+            )
+            .addSubcommand((subcommand) =>
+                subcommand
+                    .setName('city')
+                    .setDescription('Get the weather in a given city')
+                    .addStringOption((option) =>
+                        option.setName('city').setDescription('City to find the weather in').setRequired(true),
+                    ),
             );
+    }
+
+    async getWeather(lat: number, lon: number, interaction: ChatInputCommandInteraction) {
+        try {
+            const result = await this.weatherService.getCurrentWeather(lat, lon);
+
+            if (!result || !result.data) {
+                await interaction.editReply('Could not fetch weather :(');
+                return;
+            }
+
+            const weather = result.data;
+            console.log(weather);
+
+            const info = weather.weather.pop();
+
+            const embed = new EmbedBuilder()
+                .setTitle(`Weather ${weather.name}, ${weather.sys.country}`)
+                .setColor(Colors.Green)
+                .setDescription(info.description)
+                .addFields(
+                    { name: 'Temp', value: `${weather.main.temp}°C`, inline: true },
+                    { name: 'Min', value: `${weather.main.temp_min}°C`, inline: true },
+                    { name: 'Max', value: `${weather.main.temp_max}°C`, inline: true },
+                    { name: 'Feels Like', value: `${weather.main.feels_like}°C`, inline: true },
+                    { name: 'Pressure', value: `${weather.main.pressure}hPa`, inline: true },
+                    { name: 'Humidity', value: `${weather.main.humidity}%`, inline: true },
+                )
+                .setThumbnail(`http://openweathermap.org/img/wn/${info.icon}@2x.png`);
+
+            await interaction.editReply({ embeds: [embed] });
+        } catch (e) {
+            console.error(e);
+        }
     }
 }
--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -2,8 +2,9 @@ import ServiceInterface from '../service.interface';
 import { Service } from 'typedi';
 import EnvironmentService from './environment.service';
 import axios from 'axios';
+import { URLSearchParams } from 'url';
 
-const URL = 'https://api.openweathermap.org/data/2.5/';
+const URL = 'https://api.openweathermap.org/';
 
 @Service()
 export default class WeatherService implements ServiceInterface {
@@ -18,6 +19,12 @@ export default class WeatherService implements ServiceInterface {
     async getCurrentWeather(lat: number, lon: number) {
         const options = { lat: lat.toString(), lon: lon.toString(), appid: this.api, units: 'metric' };
         const params = new URLSearchParams(options);
-        return await axios.get(`${URL}weather?${params}`);
+        return await axios.get(`${URL}data/2.5/weather?${params}`);
+    }
+
+    async geoCoding(city: string) {
+        const options = { q: city, limit: '1', appid: this.api };
+        const params = new URLSearchParams(options);
+        return await axios.get(`${URL}geo/1.0/direct?${params}`);
     }
 }
